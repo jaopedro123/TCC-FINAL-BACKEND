94b85c6c332d60e838911c2214b8d1c668197b58
MyorniS <murilodsilva57fe@gmail.com>
2024-08-24 14:35:41 -0300

    Só pra conseguir fazer um negócio

--- MotherBoardWeb/MotherBoardBack-End/src/main/java/com/MotherBoard/Admin/categoria/CategoriaControlador.java
+++ MotherBoardWeb/MotherBoardBack-End/src/main/java/com/MotherBoard/Admin/categoria/CategoriaControlador.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.MotherBoard.Admin.FileUploadUtil;
 import com.MotherBoard.entidade.comum.Categoria;
+import com.MotherBoard.entidade.comum.Usuario;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
